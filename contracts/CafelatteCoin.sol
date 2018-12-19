4e4107158bca5388657ebca2e48ab44307ab31bd
thorpe <koangbok@gmail.com>
2018-12-19 14:22:56 +0900

    cafelatte coin 만들기

--- contracts/CafelatteCoin.sol
+++ contracts/CafelatteCoin.sol
@@ -1,14 +1,16 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.23;
+
 import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract CafelatteCoin is StandardToken {
-  string public name = "CafelatteCoin";
-  string public symbol = "CLC";
-  uint public decimals = 2;
-  uint public INITIAL_SUPPLY = 10000 * (10 ** decimals);
+    string public name = "CafelatteCoin";
+    string public symbol = "CLC";
+    uint8 public constant decimals = 18;
+    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
 
-  function CafelatteCoin() public {
-    totalSupply = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-  }
+    constructor() public {
+        uint256 totalSupply;
+        totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+    }
 }
--- truffle.js
+++ truffle.js
@@ -4,11 +4,11 @@ const web3 = new Web3();
 const WalletProvider = require("truffle-wallet-provider");
 const Wallet = require('ethereumjs-wallet');
 
-var mainNetPrivateKey = new Buffer(process.env["MAINNET_PRIVATE_KEY"], "hex")
+var mainNetPrivateKey = new Buffer(process.env.MAINNET_PRIVATE_KEY, "hex")
 var mainNetWallet = Wallet.fromPrivateKey(mainNetPrivateKey);
 var mainNetProvider = new WalletProvider(mainNetWallet, "https://mainnet.infura.io/");
 
-var ropstenPrivateKey = new Buffer(process.env["ROPSTEN_PRIVATE_KEY"], "hex")
+var ropstenPrivateKey = new Buffer(process.env.ROPSTEN_PRIVATE_KEY, "hex")
 var ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
 var ropstenProvider = new WalletProvider(ropstenWallet, "https://ropsten.infura.io/");
 
@@ -17,7 +17,7 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 7545,
       network_id: "*" // Match any network id
     },
     ropsten: {
